23016e6a8f163128e1c128acc5e7fed97b7ea324
im-yjoshua <yashoozerjoshua@gmail.com>
2024-08-24 23:01:09 +0500

    initial

--- weather_app.py
+++ weather_app.py
@@ -1,27 +1,12 @@
-# import requests
-
-# def weather_Extractor(city):
-#     api_key = '00e4a662750c6701379d9e1fcbb29d1c'
-#     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
-#     response = requests.get(url)
-#     data = response.json()
-#     todaySky, tempRN, tempFeelsLike, humidity, windSpeed, location  = data['weather'][0]['main'], data['main']['temp'], data['main']['feels_like'], data['main']['humidity'], data['wind']['speed'], data['name']
-
-#     return f"Today the sky will be {todaySky}.\nThe temperature right now is {tempRN}℃   but it feels like {tempFeelsLike}℃.\nThe humidity today is {humidity} %, and the wind speed right now {windSpeed} m/s, in {location}."
-
-
-# if __name__ == "__main__":
-#     print("Enter the city name")
-#     cityLocation = input("Enter here: ")
-#     print(weather_Extractor(cityLocation))
-
-
 import streamlit as st
 import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
+api_key = os.getenv('API_KEY')
 
 
 def weather_extractor(city):
-    api_key = '00e4a662750c6701379d9e1fcbb29d1c'
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
     response = requests.get(url)
     data = response.json()
